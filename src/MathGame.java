5412b92f0c1411c5cab8439e616e549cb6de4200
jeremy aubrey <jeremyaubrey@gmail.com>
2022-03-22 12:37:43 -0500

    implemented playRound and upgradeDifficulty

--- src/MathGame.java
+++ src/MathGame.java
@@ -21,56 +21,131 @@ import java.util.Scanner;
 
 public class MathGame {
 	
-	Scanner input = new Scanner(System.in);
+	private Scanner input = new Scanner(System.in);
+	private MathQuestionGenerator qGenerator = new BasicQuestionGenerator();
+	private MathGrader grader = new MathGrader();
+	private int score = 0;
 	
 	public static void main(String[] args) {
 		
 		MathGame game = new MathGame();
 		game.developerInfo();
 		
-		//get grader
-		MathGrader grader = new MathGrader();
+		boolean isPlaying = true;
 		
-		// get a MathQuestionGenerator
-		MathQuestionGenerator qGenerator = new BasicQuestionGenerator();
-		
-		String question = qGenerator.newQuestion();
-		
-		while (true) {
+		while (isPlaying) {
 			
-			int ans;
-			try {
-				
-				System.out.println(question);
-				ans = Integer.parseInt(game.getUserAnswer());
-				int expected = qGenerator.getCurrentAnswer();
-				boolean isCorrect = grader.grade(ans, expected);
-				System.out.println(grader.getRandomResponse(isCorrect));
- 				
-				if(isCorrect) {
-					System.out.println("getting new question");
- 					question = qGenerator.newQuestion();
- 					System.out.println(qGenerator.getCurrentQuestion());
- 				}
-				
-				
-			} catch (NumberFormatException e) {
-
-				e.printStackTrace();
-			}
+			game.playRound(); // score ++ 
+//			isPlaying = game.upGradeOrQuit();
 		}
 		
 		
 	}
 	
-	public String getUserAnswer() {
+	public void playRound() {
+
+		String question = qGenerator.newQuestion();
+		System.out.println(question);
+		
+		int userAnswer = getValidInt();
+		int expectedAnswer = qGenerator.getCurrentAnswer();
+		boolean isCorrect = grader.grade(expectedAnswer, userAnswer);
+		System.out.println(grader.getRandomResponse(isCorrect));
+		
+		while(!isCorrect) { // keep asking same question until user gets correct
+			
+			System.out.println(question);
+			userAnswer = getValidInt();
+			isCorrect = grader.grade(expectedAnswer, userAnswer);
+			System.out.println(grader.getRandomResponse(isCorrect));
+
+		}
+		
+		score++; // user has entered the correct answer, increment score
+	}
+	
+	public int getScore() {
+		return this.score;
+	}
+	
+	public boolean isUpgradable() {
+		
+		boolean isUpgradable = false;
+		if(getScore() >= 5 && !qGenerator.getDifficultyLevel().equals("Advanced")) {
+			isUpgradable = true;
+		}
+		
+		return isUpgradable;
+	}
+	
+	public boolean upGradeOrQuit() {
+		
+		boolean quit = false;
+		
+		if(qGenerator.getDifficultyLevel().equals("Advanced")) {
+			// allow user to quit only
+			
+		} else { // difficulty is either basic or intermediate 
+			
+			
+			
+		}
+		
+		return quit;
+	}
+	
+	public void upGradeDifficulty() {
+		
+		String oldDifficulty = qGenerator.getDifficultyLevel();
+		
+		if(oldDifficulty.equals("Basic")) { // Basic -> Intermediate
+		
+			this.qGenerator = new IntermediateQuestionGenerator();
+			
+		} else if(oldDifficulty.equals("Intermediate")) { // Intermediate -> Advanced
+			
+			this.qGenerator = new AdvancedQuestionGenerator();
+		}
+		
+		String newDifficulty = qGenerator.getDifficultyLevel();
+		
+		System.out.println("Difficulty upgraded from [ " + oldDifficulty + " ] -> [ " + newDifficulty + " ]");
+		
+	}
+	
+	public String getUserInput(String promptMsg) {
 		
 		String answer = "";
-		System.out.print("Please enter a number: ");
+		System.out.print(promptMsg);
 		answer = input.nextLine();
 
 		return answer;
 		
+	}
+	
+	public int getValidInt() {
+		
+		boolean isValid = false;
+		int answer = 0;
+		
+		while(!isValid) {
+			
+			System.out.print("Answer: ");
+			String userAnswer = input.nextLine();
+			
+			try {
+				
+				answer = Integer.parseInt(userAnswer);
+				isValid = true;
+				
+			} catch (NumberFormatException e) {
+				
+				System.out.println("Must enter an integer");
+			}
+		}
+		
+		return answer;
+
 	}
 	
 	
